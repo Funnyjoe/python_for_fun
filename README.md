53129e7065883b951323b86b9556c47752a25406
Funnyjoe <38206938+Funnyjoe@users.noreply.github.com>
2020-10-31 11:12:46 +0800

    Supports most of the Asian languages.

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 # python_for_fun
 # First, make sure you installed python2 or python3  in your computer.
 # Second, intall pyttsx3 in your computer, pip install pyttsx3 for python2, pip3 intall pyttsx3 for python3.
-# Third, install pypdf2, pip install pypdf2 for python2, pip3 intall pyttsx3 for pypdf2.
-# pypdf2 is not works for Chinese.
+# Third, install pdfminer or pdfminer.six, pip install pdfminer for python2, pip3 intall pdfminer.six for python3.
--- audiobook.py
+++ audiobook.py
@@ -1,20 +1,29 @@
-import pyttsx3
-import PyPDF2
+from io import StringIO
 
-book = open('Untitled.pdf', 'rb')
-# "r" - Read - Default value. Opens a file for reading, error if the file does not exist
-# "b" - Binary - Binary mode (e.g. images)
-pdfReader = PyPDF2.PdfFileReader(book)
-pages = pdfReader.numPages
-print(pages)
-TTSapp = pyttsx3.init()
-TTSapp.setProperty('rate', 120)
-for num in range(0, pages):
-    # You can set the range of pages.
-    page = pdfReader.getPage(num)
-    text = page.extractText()
-    print(text)
-    TTSapp.say("Let's start to read page {}".format(num))
-    TTSapp.say(text)
-    TTSapp.runAndWait()
-    TTSapp.stop
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
+from pdfminer.pdfdocument import PDFDocument
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.pdfpage import PDFPage
+from pdfminer.pdfparser import PDFParser
+import pyttsx3
+
+Speaker = pyttsx3.init()
+Speaker.setProperty('rate', 120)
+output_string = StringIO()
+with open('Untitled.pdf', 'rb') as in_file:
+    parser = PDFParser(in_file)
+    doc = PDFDocument(parser)
+    rsrcmgr = PDFResourceManager()
+    device = TextConverter(rsrcmgr, output_string, laparams=LAParams())
+    interpreter = PDFPageInterpreter(rsrcmgr, device)
+    page_num = 1
+    for page in PDFPage.create_pages(doc):
+        interpreter.process_page(page)
+        print(page)
+        Speaker.say("Let's start to read page {}".format(page_num))
+        print(output_string.getvalue())
+        Speaker.say(output_string.getvalue())
+        Speaker.runAndWait()
+        Speaker.stop
+        page_num += 1
